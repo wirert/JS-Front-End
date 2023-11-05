2f5133604a2ce9ad0b15d33eece233e9d46c3cab
Krasimir Nikolov <wirert@gmail.com>
2023-11-05 10:49:40 +0200

    func more exercises 4.Radio crystals

--- 03.2.Functions and Statements - More Exercises/04. Radio Crystals.js
+++ 03.2.Functions and Statements - More Exercises/04. Radio Crystals.js
@@ -1,39 +1,13 @@
 function solve(arr) {
-  let operations = {
+  const operations = {
     Cut: (x) => x / 4,
     Lap: (x) => x * 0.8,
     Grind: (x) => x - 20,
     Etch: (x) => x - 2,
     "X-ray": (x) => x + 1,
-    //washAndTransportCrystal: (x) => Math.floor(x),
   };
-  // let operationName;
-  // function chooseOperation(thickness, crystal) {
-  //   if (crystal / 4 >= thickness - 1) {
-  //     operationName = "Cut";
-  //     return crystal / 4;
-  //   }
-  //   if (crystal * 0.8 >= thickness - 1) {
-  //     operationName = "Lap";
-  //     return (crystal *= 0.8);
-  //   }
-  //   if (crystal - 20 >= thickness - 1) {
-  //     operationName = "Grind";
-  //     return (crystal -= 20);
-  //   }
-  //   if (crystal - 2 >= thickness - 1) {
-  //     operationName = "Etch";
-  //     return (crystal -= 2);
-  //   }
-  //   if (crystal - 1 === thickness) {
-  //     operationName = "X-ray";
-  //     return (crystal += 1);
-  //   }
 
-  //   return null;
-  // }
-
-  let [thickness, ...crystals] = arr;
+  const [thickness, ...crystals] = arr;
 
   for (const stone of crystals) {
     let crystal = stone;
@@ -45,69 +19,45 @@ function solve(arr) {
     }
 
     let priviousOperationName;
-    let count = 0;
-    while (true) {
-      if (crystal === thickness) {
-        console.log(`${priviousOperationName} x${count}`);
-        break;
-      }
-      let currOperationName;
+    let count = 1;
+    while (crystal !== thickness) {
+      const currOperationName = chooseProperOperation(crystal, thickness);
+      crystal = operations[currOperationName](crystal);
 
-      if (crystal / 4 >= thickness - 1) {
-        currOperationName = "Cut";
-      } else if (crystal * 0.8 >= thickness - 1) {
-        currOperationName = "Lap";
-      } else if (crystal - 20 >= thickness - 1) {
-        currOperationName = "Grind";
-      } else if (crystal - 2 >= thickness - 1) {
-        currOperationName = "Etch";
-      } else if (crystal - 1 === thickness) {
-        currOperationName = "X-ray";
-      }
-
-      let operation = operations[currOperationName](crystal);
-
-      if (
-        priviousOperationName &&
-        currOperationName !== priviousOperationName
-      ) {
+      if (currOperationName === priviousOperationName) {
+        count++;
+      } else if (priviousOperationName) {
         console.log(`${priviousOperationName} x${count}`);
         console.log("Transporting and washing");
-
         crystal = Math.floor(crystal);
-        if (crystal === thickness) break;
-        crystal = operation;
-
-        priviousOperationName = currOperationName;
         count = 1;
-      } else {
-        crystal = operation;
-        count++;
       }
+
+      priviousOperationName = currOperationName;
     }
 
+    console.log(`${priviousOperationName} x${count}`);
+    if (priviousOperationName !== "X-ray") {
+      console.log("Transporting and washing");
+    }
     console.log(`Finished crystal ${thickness} microns`);
+  }
+
+  function chooseProperOperation(crystal, thickness) {
+    if (crystal / 4 >= thickness - 1) {
+      operation = "Cut";
+    } else if (crystal * 0.8 >= thickness - 1) {
+      operation = "Lap";
+    } else if (crystal - 20 >= thickness - 1) {
+      operation = "Grind";
+    } else if (crystal - 2 >= thickness - 1) {
+      operation = "Etch";
+    } else if (crystal === thickness - 1) {
+      operation = "X-ray";
+    }
+
+    return Object.values(operation).join("");
   }
 }
 
 solve([1375, 50000]);
-
-// if (crystal === thickness) {
-//   console.log(`${operationName} x${count}`);
-//   break;
-// }
-// let currOperation = chooseOperation(thickness, crystal);
-// if (operation && currOperation !== operation) {
-//   console.log(`${operationName} x${count}`);
-//   console.log("Transporting and washing");
-
-//   crystal = Math.floor(crystal);
-//   if (crystal === thickness) break;
-//   crystal = currOperation(crystal);
-
-//   operation = currOperation;
-//   count = 1;
-// } else {
-//   crystal = currOperation(crystal);
-//   count++;
-// }
