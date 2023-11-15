bcba6af0fb66e8cedcd01aa8908b1a0e928d4777
Krasimir Nikolov <wirert@gmail.com>
2023-11-15 19:49:53 +0200

    04.1.08. Piccolo

---
+++ 04.1.Objects and Classes - Exercises/08. Piccolo.js
@@ -0,0 +1,36 @@
+function printCarsInParking(input) {
+  const parking = input.reduce((acc, curr) => {
+    let [action, carNumber] = curr.split(", ");
+    if (action === "IN") {
+      acc[carNumber] = carNumber;
+    } else {
+      acc[carNumber] = undefined;
+    }
+    return acc;
+  }, {});
+  let result = Object.values(parking)
+    .filter((number) => {
+      return number !== undefined;
+    })
+    .sort();
+
+  if (result.length === 0) {
+    console.log("Parking Lot is Empty");
+    return;
+  }
+
+  console.log(result.join("\n"));
+}
+
+printCarsInParking([
+  "IN, CA2844AA",
+  "IN, CA1234TA",
+  "OUT, CA2844AA",
+  "IN, CA9999TT",
+  "IN, CA2866HI",
+  "OUT, CA1234TA",
+  "IN, CA2844AA",
+  "OUT, CA2866HI",
+  "IN, CA9876HH",
+  "IN, CA2822UU",
+]);
