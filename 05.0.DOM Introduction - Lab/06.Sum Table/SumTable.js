624a4a19853700d30e435e773985f51f6df7dac9
Krasimir Nikolov <wirert@gmail.com>
2023-11-20 18:20:14 +0200

    05.0.06. Sum Table

--- 05.0.DOM Introduction - Lab/06.Sum Table/SumTable.js
+++ 05.0.DOM Introduction - Lab/06.Sum Table/SumTable.js
@@ -1,3 +1,10 @@
 function sumTable() {
-
+  let elements = Array.from(
+    document.querySelectorAll("td:nth-child(even):not(#sum)")
+  );
+  let result = elements.reduce(
+    (acc, curr) => (acc += Number(curr.textContent)),
+    0
+  );
+  document.getElementById("sum").textContent = result;
 }
