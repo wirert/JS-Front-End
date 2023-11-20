cb40d78e796f0ee5a3aa29648e376b2e5b677ee2
Krasimir Nikolov <wirert@gmail.com>
2023-11-20 17:43:15 +0200

    05.0.02. Collect List

--- 05.0.DOM Introduction - Lab/02.Collect List Items/CollectListItems.js
+++ 05.0.DOM Introduction - Lab/02.Collect List Items/CollectListItems.js
@@ -1,3 +1,8 @@
 function extractText() {
-    // TODO
+  let elements = Array.from(document.querySelectorAll("#items>li"));
+  let result = elements.reduce(
+    (acc, curr) => (acc += curr.textContent + "\n"),
+    ""
+  );
+  document.querySelector("#result").textContent = result;
 }
