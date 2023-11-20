c6f8b09e80eed636099ac4fb6bf9c7a5eced9e37
Krasimir Nikolov <wirert@gmail.com>
2023-11-20 19:01:14 +0200

    05.0.07. Extract Parenthesis

--- 05.0.DOM Introduction - Lab/07.Extract Parenthesis/ExtractParenthesis.js
+++ 05.0.DOM Introduction - Lab/07.Extract Parenthesis/ExtractParenthesis.js
@@ -1,3 +1,10 @@
 function extract(content) {
-
+  const regEx = /\(([^)]+)\)/gm;
+  let text = document.querySelector(`#${content}`).textContent;
+  let matches = [];
+  let array;
+  while ((array = regEx.exec(text)) !== null) {
+    matches.push(array[1]);
+  }
+  return matches.join("; ");
 }
