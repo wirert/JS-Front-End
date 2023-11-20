00ffcc9242ec79d325859a864a595cc8ac9c30f8
Krasimir Nikolov <wirert@gmail.com>
2023-11-20 20:17:51 +0200

    05.1.02. Pascal or Camel Case

--- 05.1.DOM-Introduction-Exercise/02. Pascal or Camel Case/solution.js
+++ 05.1.DOM-Introduction-Exercise/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,22 @@
 function solve() {
-  //TODO...
+  function stringArrayToPascalCase(array) {
+    return array.reduce((acc, curr) => {
+      return acc + curr.charAt(0).toUpperCase() + curr.slice(1);
+    }, "");
+  }
+
+  let textArr = document.getElementById("text").value.toLowerCase().split(" ");
+  let convention = document.getElementById("naming-convention").value;
+  let result;
+
+  if (convention === "Camel Case") {
+    let [first, ...rest] = textArr;
+    result = first + stringArrayToPascalCase(rest);
+  } else if (convention === "Pascal Case") {
+    result = stringArrayToPascalCase(textArr);
+  } else {
+    result = "Error!";
+  }
+
+  document.getElementById("result").textContent = result;
 }
