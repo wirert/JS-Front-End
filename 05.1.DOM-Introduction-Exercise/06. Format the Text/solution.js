0f76d003ca29ff36a5995166f63232cb31553584
Krasimir Nikolov <wirert@gmail.com>
2023-11-21 17:23:22 +0200

    05.1.06. Format the Text

--- 05.1.DOM-Introduction-Exercise/06. Format the Text/solution.js
+++ 05.1.DOM-Introduction-Exercise/06. Format the Text/solution.js
@@ -1,3 +1,18 @@
 function solve() {
-  //TODO
+  let inputElement = document.getElementById("input").value;
+  let sentences = inputElement.split(".");
+  sentences = sentences.filter((s) => s.length > 0);
+  let output = document.getElementById("output");
+  output.textContent = "";
+  let curParagraphText = "";
+  let i = 0;
+  sentences.forEach((sentence, i) => {
+    curParagraphText += sentence + ".";
+    if ((i + 1) % 3 === 0 || i === sentences.length - 1) {
+      let paragraph = document.createElement("p");
+      paragraph.textContent = curParagraphText;
+      output.appendChild(paragraph);
+      curParagraphText = "";
+    }
+  });
 }
