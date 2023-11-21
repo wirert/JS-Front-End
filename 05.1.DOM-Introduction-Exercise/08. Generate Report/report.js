64424cfdeade73e8a3118888a90c89a15ae94db9
Krasimir Nikolov <wirert@gmail.com>
2023-11-21 20:50:29 +0200

    05.1.08. Generate Report

--- 05.1.DOM-Introduction-Exercise/08. Generate Report/report.js
+++ 05.1.DOM-Introduction-Exercise/08. Generate Report/report.js
@@ -1,3 +1,30 @@
 function generateReport() {
-    //TODO
+  function takeIndicesAndNamesOfCheckedColumns() {
+    let theadChildern = Array.from(document.querySelectorAll("thead th input"));
+    let result = [];
+    theadChildern.forEach((element, index) => {
+      if (element.checked) {
+        result.push({ index, name: element.name });
+      }
+    });
+    return result;
+  }
+
+  const indicesAndNames = takeIndicesAndNamesOfCheckedColumns();
+  let rows = Array.from(document.querySelectorAll("tbody tr"));
+  let result = rows.map((row) => {
+    let resultRow = {};
+    Array.from(row.children).forEach((element, index) => {
+      let indexAndName = indicesAndNames.find((i) => i.index === index);
+
+      if (indexAndName) {
+        resultRow[indexAndName.name] = element.textContent;
+      }
+    });
+    return resultRow;
+  });
+
+  let outputTextArea = document.querySelector("#output");
+
+  outputTextArea.value = JSON.stringify(result);
 }
