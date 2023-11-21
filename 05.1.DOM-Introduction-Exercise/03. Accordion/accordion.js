daa01ed3a1aa7c7e21a9ce648246ae3026f97d34
Krasimir Nikolov <wirert@gmail.com>
2023-11-21 13:52:03 +0200

    05.1.03. Accordion

--- 05.1.DOM-Introduction-Exercise/03. Accordion/accordion.js
+++ 05.1.DOM-Introduction-Exercise/03. Accordion/accordion.js
@@ -1,3 +1,10 @@
 function toggle() {
-    console.log('TODO:...');
+  let buttonText = document.querySelector("span.button").textContent;
+  if (buttonText === "More") {
+    document.querySelector("span.button").textContent = "Less";
+    document.querySelector("#extra").style.display = "block";
+  } else {
+    document.querySelector("span.button").textContent = "More";
+    document.querySelector("#extra").style.display = "none";
+  }
 }
