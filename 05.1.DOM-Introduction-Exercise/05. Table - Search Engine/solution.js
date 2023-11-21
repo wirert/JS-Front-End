bbb48bf54bda746fec36de4cfbaa10df8ec8cb65
Krasimir Nikolov <wirert@gmail.com>
2023-11-21 15:25:46 +0200

    05.1.05. Table – Search Engine

--- 05.1.DOM-Introduction-Exercise/05. Table - Search Engine/solution.js
+++ 05.1.DOM-Introduction-Exercise/05. Table - Search Engine/solution.js
@@ -1,8 +1,21 @@
 function solve() {
-   document.querySelector('#searchBtn').addEventListener('click', onClick);
+  document.querySelector("#searchBtn").addEventListener("click", onClick);
 
-   function onClick() {
-      //   TODO:
+  function onClick() {
+    let rows = Array.from(document.querySelectorAll("tbody>tr"));
+    let searchField = document.querySelector("#searchField");
+    rows.forEach((row) => {
+      row.className = "";
+      Array.from(row.children).forEach((child) => {
+        if (
+          child.textContent.includes(searchField.value) &&
+          row.className === ""
+        ) {
+          row.className = "select";
+        }
+      });
+    });
 
-   }
+    searchField.value = "";
+  }
 }
