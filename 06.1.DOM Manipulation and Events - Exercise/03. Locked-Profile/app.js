e4dd6d2f4f3fe568f01b4682400dc2e726c00689
Krasimir Nikolov <wirert@gmail.com>
2023-11-22 19:36:29 +0200

    06.1.03. Locked Profile

--- 06.1.DOM Manipulation and Events - Exercise/03. Locked-Profile/app.js
+++ 06.1.DOM Manipulation and Events - Exercise/03. Locked-Profile/app.js
@@ -1,3 +1,26 @@
 function lockedProfile() {
-    console.log('TODO...')
+  let buttons = Array.from(document.querySelectorAll("button"));
+
+  buttons.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      let radioLock = e.target.parentElement.querySelector(
+        'input[value="lock"]'
+      );
+      if (radioLock.checked === true) {
+        return;
+      }
+
+      if (btn.innerText === "Show more") {
+        btn.innerText = "Hide it";
+        e.target.parentElement.querySelector(
+          'div[id$="HiddenFields"'
+        ).style.display = "block";
+      } else {
+        btn.innerText = "Show more";
+        e.target.parentElement.querySelector(
+          'div[id$="HiddenFields"'
+        ).style.display = "none";
+      }
+    });
+  });
 }
