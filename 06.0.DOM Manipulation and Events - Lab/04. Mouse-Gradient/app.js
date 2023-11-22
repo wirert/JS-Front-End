12883e02a41f7ae90a4df5000ea0b5abe331be9d
Krasimir Nikolov <wirert@gmail.com>
2023-11-22 14:04:02 +0200

    06.0.04. Mouse Gradient

--- 06.0.DOM Manipulation and Events - Lab/04. Mouse-Gradient/app.js
+++ 06.0.DOM Manipulation and Events - Lab/04. Mouse-Gradient/app.js
@@ -1,3 +1,10 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+  let hoverElement = document.getElementById("gradient");
+  let resultDiv = document.getElementById("result");
+  hoverElement.addEventListener("mouseout", () => (resultDiv.textContent = ""));
+  hoverElement.addEventListener("mousemove", (e) => {
+    let percent = e.offsetX / e.target.clientWidth;
+    percent = Math.trunc(percent * 100);
+    resultDiv.textContent = percent + "%";
+  });
 }
