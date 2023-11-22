ba0bef0ae88b1449c3a53225ba202e96379e0a99
Krasimir Nikolov <wirert@gmail.com>
2023-11-22 19:44:53 +0200

    06.1.04. Fill Dropdown

--- 06.1.DOM Manipulation and Events - Exercise/04. Fill-Dropdown/dropdown.js
+++ 06.1.DOM Manipulation and Events - Exercise/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,12 @@
 function addItem() {
-    console.log('TODO:...');
+  let itemText = document.getElementById("newItemText");
+  let itemValue = document.getElementById("newItemValue");
+
+  let option = document.createElement("option");
+  option.innerText = itemText.value;
+  option.value = itemValue.value;
+  itemText.value = "";
+  itemValue.value = "";
+
+  document.getElementById("menu").appendChild(option);
 }
