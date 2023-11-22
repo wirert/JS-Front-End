f86740ba02d20981b790bf15fce36c1406d99adb
Krasimir Nikolov <wirert@gmail.com>
2023-11-22 16:38:41 +0200

    06.0.06. Dynamic Validation

--- 06.0.DOM Manipulation and Events - Lab/06. Dynamic-Validation/app.js
+++ 06.0.DOM Manipulation and Events - Lab/06. Dynamic-Validation/app.js
@@ -1,3 +1,11 @@
 function validate() {
-    console.log('TODO:...');
+  let emailInput = document.querySelector("#email");
+  emailInput.addEventListener("change", (e) => {
+    let regEx = /^[a-z\-\.]+@([a-z-]+\.)+[a-z-]{2,}$/;
+    if (regEx.test(e.target.value)) {
+      e.target.classList.remove("error");
+    } else {
+      e.target.classList.add("error");
+    }
+  });
 }
