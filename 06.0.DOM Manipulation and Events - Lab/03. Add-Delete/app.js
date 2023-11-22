185a6af740c188853820339b73e31bd37789de0b
Krasimir Nikolov <wirert@gmail.com>
2023-11-22 13:34:53 +0200

    06.0.03. Add / Delete

--- 06.0.DOM Manipulation and Events - Lab/03. Add-Delete/app.js
+++ 06.0.DOM Manipulation and Events - Lab/03. Add-Delete/app.js
@@ -1,3 +1,13 @@
 function addItem() {
-    //TODO...
+  let newItemText = document.querySelector("#newItemText").value;
+  let newLi = document.createElement("li");
+  newLi.textContent = newItemText;
+  let deleteAnchor = document.createElement("a");
+  deleteAnchor.href = "#";
+  deleteAnchor.text = "[Delete]";
+  deleteAnchor.addEventListener("click", () => {
+    deleteAnchor.parentElement.remove();
+  });
+  newLi.appendChild(deleteAnchor);
+  document.querySelector("#items").appendChild(newLi);
 }
