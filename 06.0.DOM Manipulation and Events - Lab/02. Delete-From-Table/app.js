de3d2b10c7a584db306649465a1c9fddc5e68d1d
Krasimir Nikolov <wirert@gmail.com>
2023-11-22 13:14:47 +0200

    06.0.02. Delete from Table

--- 06.0.DOM Manipulation and Events - Lab/02. Delete-From-Table/app.js
+++ 06.0.DOM Manipulation and Events - Lab/02. Delete-From-Table/app.js
@@ -1,3 +1,18 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+  let inputEmail = document.querySelector('input[name="email"]').value;
+
+  let tableEmails = Array.from(
+    document.querySelectorAll("#customers td:nth-child(even)")
+  );
+
+  let emailToDelete = tableEmails.find((e) => e.textContent === inputEmail);
+  let result;
+  if (emailToDelete) {
+    emailToDelete.parentElement.remove();
+    result = "Deleted.";
+  } else {
+    result = "Not found.";
+  }
+
+  document.querySelector("#result").textContent = result;
 }
