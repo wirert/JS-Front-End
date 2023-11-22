e71b60d81b1187be7182f58e733eb31e94a096f1
Krasimir Nikolov <wirert@gmail.com>
2023-11-22 16:20:50 +0200

    06.0.05. Highlight Active

--- 06.0.DOM Manipulation and Events - Lab/05. Highlight-Active/app.js
+++ 06.0.DOM Manipulation and Events - Lab/05. Highlight-Active/app.js
@@ -1,3 +1,11 @@
 function focused() {
-    console.log('TODO:...');
+  let inputs = Array.from(document.querySelectorAll("input"));
+  inputs.forEach((input) => {
+    input.addEventListener("focus", (e) => {
+      e.target.parentElement.classList.add("focused");
+    });
+    input.addEventListener("blur", (e) => {
+      e.target.parentElement.classList.remove("focused");
+    });
+  });
 }
