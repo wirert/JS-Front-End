8567220a0b2136a3684392f62503530e22e8bf35
Krasimir Nikolov <wirert@gmail.com>
2023-11-22 21:12:34 +0200

    06.1.05. Encode and Decode Messages

--- 06.1.DOM Manipulation and Events - Exercise/05. Encode-and-Decode-Messages/app.js
+++ 06.1.DOM Manipulation and Events - Exercise/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,22 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+  let [encodeBtn, decodeBtn] = Array.from(document.querySelectorAll("button"));
+  let [encodeArea, decodeArea] = Array.from(
+    document.querySelectorAll("textarea")
+  );
+  encodeBtn.addEventListener("click", () => {
+    decodeArea.value = codeText(encodeArea.value, 1);
+    encodeArea.value = "";
+  });
+
+  decodeBtn.addEventListener("click", () => {
+    decodeArea.value = codeText(decodeArea.value, -1);
+  });
+
+  function codeText(text, salt) {
+    let textArr = text.split("").map((char) => {
+      let intChar = char.charCodeAt() + salt;
+      return String.fromCharCode(intChar);
+    });
+    return textArr.join("");
+  }
 }
