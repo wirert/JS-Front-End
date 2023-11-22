6c57ee5b0919d5af52c8b486060c1fe54aebab1c
Krasimir Nikolov <wirert@gmail.com>
2023-11-22 18:50:50 +0200

    06.1.02. Time Converter

--- 06.1.DOM Manipulation and Events - Exercise/02. Time-Converter/solution.js
+++ 06.1.DOM Manipulation and Events - Exercise/02. Time-Converter/solution.js
@@ -1,4 +1,30 @@
 function attachEventsListeners() {
+  let daysInput = document.getElementById("days");
+  let daysBtn = document.getElementById("daysBtn");
+  let hoursInput = document.getElementById("hours");
+  let hoursBtn = document.getElementById("hoursBtn");
+  let minutesInput = document.getElementById("minutes");
+  let minutesBtn = document.getElementById("minutesBtn");
+  let secondsInput = document.getElementById("seconds");
+  let secondsBtn = document.getElementById("secondsBtn");
 
-    console.log('TODO:...');
+  daysBtn.addEventListener("click", () =>
+    convertTimeAndDisplay(Number(daysInput.value * 1440))
+  );
+  hoursBtn.addEventListener("click", () =>
+    convertTimeAndDisplay(Number(hoursInput.value * 60))
+  );
+  minutesBtn.addEventListener("click", () =>
+    convertTimeAndDisplay(Number(minutesInput.value))
+  );
+  secondsBtn.addEventListener("click", () =>
+    convertTimeAndDisplay(Number(secondsInput.value / 60))
+  );
+
+  function convertTimeAndDisplay(minutes) {
+    daysInput.value = minutes / 1440;
+    hoursInput.value = minutes / 60;
+    minutesInput.value = minutes;
+    secondsInput.value = minutes * 60;
+  }
 }
