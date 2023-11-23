50cb3445acd02e6e39454b04cb81b3c6fab6daf2
Krasimir Nikolov <wirert@gmail.com>
2023-11-23 16:17:08 +0200

    06.1.08. Sudomu

--- 06.1.DOM Manipulation and Events - Exercise/08. Sudomu/solution.js
+++ 06.1.DOM Manipulation and Events - Exercise/08. Sudomu/solution.js
@@ -1,4 +1,60 @@
 function solve() {
+  let [checkBtn, clearBtn] = Array.from(
+    document.querySelectorAll("tfoot button")
+  );
 
-    //TODO...
+  clearBtn.addEventListener("click", clearTable);
+  checkBtn.addEventListener("click", checkGame);
+
+  let resultP = document.querySelector("#check p");
+  let table = document.querySelector("table");
+  let cells = Array.from(document.querySelectorAll("tbody td input"));
+
+  function clearTable() {
+    cells.forEach((c) => (c.value = ""));
+    resultP.textContent = "";
+    table.style.border = "none";
+  }
+
+  function checkGame() {
+    if (validateGame()) {
+      resultP.textContent = "You solve it! Congratulations!";
+      resultP.style.color = "green";
+      table.style.border = "2px solid green";
+      console.log("correct");
+    } else {
+      console.log("not correnct");
+      resultP.textContent = "NOP! You are not done yet...";
+      resultP.style.color = "red";
+      table.style.border = "2px solid red";
+    }
+  }
+
+  function validateGame() {
+    let elements = [];
+    let curRow = [];
+
+    for (const cell of cells) {
+      curRow.push(cell.value);
+      if (curRow.length == 3) {
+        if (new Set([...curRow]).size < 3) {
+          return false;
+        }
+        elements.push([...curRow]);
+        curRow = [];
+      }
+    }
+
+    for (let i = 0; i < 3; i++) {
+      if (
+        elements[0][i] === elements[1][i] ||
+        elements[0][i] === elements[2][i] ||
+        elements[1][i] === elements[2][i]
+      ) {
+        return false;
+      }
+    }
+
+    return true;
+  }
 }
