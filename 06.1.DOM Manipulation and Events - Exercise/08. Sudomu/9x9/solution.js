086ed0808646b330e62f752256834a591d591f3e
Krasimir Nikolov <wirert@gmail.com>
2023-11-23 18:08:27 +0200

    06.1.08. Sudocu validator

--- 06.1.DOM Manipulation and Events - Exercise/08. Sudomu/9x9/solution.js
+++ 06.1.DOM Manipulation and Events - Exercise/08. Sudomu/9x9/solution.js
@@ -2,13 +2,16 @@ function solve() {
   let [checkBtn, clearBtn] = Array.from(
     document.querySelectorAll("tfoot button")
   );
-
   clearBtn.addEventListener("click", clearTable);
   checkBtn.addEventListener("click", checkGame);
 
   let resultP = document.querySelector("#check p");
   let table = document.querySelector("table");
   let cells = Array.from(document.querySelectorAll("tbody td input"));
+  let neededSum = 0;
+  for (let i = 1; i <= 9; i++) {
+    neededSum += i;
+  }
 
   function clearTable() {
     cells.forEach((c) => (c.value = ""));
@@ -18,6 +21,7 @@ function solve() {
 
   function checkGame() {
     if (validateGame()) {
+      console.log("test");
       resultP.textContent = "You solve it! Congratulations!";
       resultP.style.color = "green";
       table.style.border = "2px solid green";
@@ -34,8 +38,8 @@ function solve() {
 
     for (const cell of cells) {
       curRow.push(cell.value);
-      if (curRow.length === 3) {
-        if (new Set([...curRow]).size < 3) {
+      if (curRow.length === 9) {
+        if (new Set([...curRow]).size < 9) {
           return false;
         }
         elements.push([...curRow]);
@@ -43,13 +47,41 @@ function solve() {
       }
     }
 
-    for (let i = 0; i < 3; i++) {
-      if (
-        elements[0][i] === elements[1][i] ||
-        elements[0][i] === elements[2][i] ||
-        elements[1][i] === elements[2][i]
-      ) {
+    if (checkSubMatrices(elements) == false || checkCols(elements) == false) {
+      return false;
+    }
+
+    return true;
+  }
+
+  function checkCols(elements) {
+    for (let col = 0; col < 9; col++) {
+      let sum = 0;
+      for (let row = 0; row < 9; row++) {
+        sum += Number(elements[row][col]);
+      }
+
+      if (sum !== neededSum) {
         return false;
+      }
+    }
+
+    return true;
+  }
+
+  function checkSubMatrices(elements) {
+    let sum = 0;
+    for (let i = 0; i < 9; i += 3) {
+      for (let j = 0; j < 9; j += 3) {
+        let sum = 0;
+        for (let row = i; row < i + 3; row++) {
+          for (let col = j; col < j + 3; col++) {
+            sum += Number(elements[row][col]);
+          }
+        }
+        if (sum !== neededSum) {
+          return false;
+        }
       }
     }
 
