2968b46cf03388730a6714a8641f2864aaa59d08
Krasimir Nikolov <wirert@gmail.com>
2023-11-23 12:34:21 +0200

    06.1.07. Distance Converter

--- 06.1.DOM Manipulation and Events - Exercise/07. Distance-Converter/distanceConverter.js
+++ 06.1.DOM Manipulation and Events - Exercise/07. Distance-Converter/distanceConverter.js
@@ -1,3 +1,22 @@
 function attachEventsListeners() {
-    console.log('TODO:...');
+  const units = {
+    km: 1000,
+    m: 1,
+    cm: 0.01,
+    mm: 0.001,
+    mi: 1609.34,
+    yrd: 0.9144,
+    ft: 0.3048,
+    in: 0.0254,
+  };
+
+  let convertBtn = document.getElementById("convert");
+  convertBtn.addEventListener("click", () => {
+    let inputUnits = document.getElementById("inputUnits").value;
+    let outputUnits = document.getElementById("outputUnits").value;
+    let count = Number(document.getElementById("inputDistance").value);
+    let outputResult = document.getElementById("outputDistance");
+
+    outputResult.value = (count * units[inputUnits]) / units[outputUnits];
+  });
 }
