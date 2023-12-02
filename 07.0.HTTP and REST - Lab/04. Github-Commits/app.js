75c8761b32920a23455c814d10190e186bfc5958
Krasimir Nikolov <wirert@gmail.com>
2023-12-02 21:57:40 +0200

    07.0.04. Github-Commits

--- 07.0.HTTP and REST - Lab/04. Github-Commits/app.js
+++ 07.0.HTTP and REST - Lab/04. Github-Commits/app.js
@@ -1,4 +1,33 @@
-function loadCommits() {
-    // Try it with Fetch API
-    console.log('TODO...');
+async function loadCommits() {
+  const commitsUl = document.getElementById("commits");
+  commitsUl.textContent = "";
+
+  try {
+    const username = document.getElementById("username").value;
+    const repo = document.getElementById("repo").value;
+
+    // if (!username || !repo) {
+    //   throw new Error("Invalid username or repo");
+    // }
+
+    const res = await fetch(
+      `https://api.github.com/repos/${username}/${repo}/commits`
+    );
+
+    if (!res.ok) {
+      throw new Error(`${res.status} (Not Found)`);
+    }
+
+    const commits = await res.json();
+
+    commits.forEach(({ commit }) => {
+      const li = document.createElement("li");
+      li.textContent = `${commit.author.name}: ${commit.message}`;
+      commitsUl.appendChild(li);
+    });
+  } catch (error) {
+    const li = document.createElement("li");
+    li.textContent = error;
+    commitsUl.appendChild(li);
+  }
 }
