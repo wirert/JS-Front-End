305f91bcbdc57c5933277873f2f90699b6caebb7
Krasimir Nikolov <wirert@gmail.com>
2023-12-04 20:24:02 +0200

    07.1.02. Bus Schedule

--- 07.1.AJAX/02.Bus-Schedule/app.js
+++ 07.1.AJAX/02.Bus-Schedule/app.js
@@ -1,16 +1,40 @@
 function solve() {
-    function depart() {
-        // TODO: 
-    }
+  let currStop = {
+    name: "",
+    next: "depot",
+  };
+  const infoBox = document.querySelector("#info span");
+  const departInput = document.querySelector("#depart");
+  const arriveInput = document.querySelector("#arrive");
 
-    async function arrive() {
-        // TODO:
+  async function depart() {
+    try {
+      currStop = await (
+        await fetch(
+          `http://localhost:3030/jsonstore/bus/schedule/${currStop.next}`
+        )
+      ).json();
+    } catch (_) {
+      updateInfo(true, true, "Error");
+      return;
     }
+    updateInfo(true, false, `Next stop ${currStop.name}`);
+  }
 
-    return {
-        depart,
-        arrive
-    };
+  function arrive() {
+    updateInfo(false, true, `Arriving at ${currStop.name}`);
+  }
+
+  function updateInfo(departBtnStatus, arriveBtnStatus, message) {
+    infoBox.textContent = message;
+    departInput.disabled = departBtnStatus;
+    arriveInput.disabled = arriveBtnStatus;
+  }
+
+  return {
+    depart,
+    arrive,
+  };
 }
 
 let result = solve();
