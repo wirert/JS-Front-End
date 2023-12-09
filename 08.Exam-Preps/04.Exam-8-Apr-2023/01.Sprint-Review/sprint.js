afc8d5f5ee7b99456b221fc01f32353abbb67f53
Krasimir Nikolov <wirert@gmail.com>
2023-12-09 14:59:45 +0200

    08.04.01.Sprint-Review -  refactoring code and index outofrange  error corrected in removeTask func

--- 08.Exam-Preps/04.Exam-8-Apr-2023/01.Sprint-Review/sprint.js
+++ 08.Exam-Preps/04.Exam-8-Apr-2023/01.Sprint-Review/sprint.js
@@ -8,9 +8,7 @@ function solve(inputArr) {
       this.assignee = assignee;
     }
   }
-
-  const [n, ...inputs] = inputArr;
-
+  const [taskCount, ...inputs] = inputArr;
   let sprintBoard = {
     "Add New": addNewTask,
     "Change Status": changeTaskStatus,
@@ -18,56 +16,68 @@ function solve(inputArr) {
   };
 
   inputs.forEach((input, i) => {
-    if (i < n) {
-      const [assignee, id, title, status, points] = input.split(":");
-      const task = new Task(id, title, status, Number(points), assignee);
-      if (!sprintBoard[assignee]) {
-        sprintBoard[assignee] = [];
-      }
-      sprintBoard[assignee].push(task);
+    i < Number(taskCount)
+      ? addNewTaskToTaskboard(input)
+      : proceedCommantToTaskBoard(input);
+  });
+
+  printResult(sprintBoard);
+
+  function addNewTaskToTaskboard(input) {
+    const [assignee, id, title, status, points] = input.split(":");
+
+    if (!sprintBoard[assignee]) {
+      sprintBoard[assignee] = [];
+    }
+    addNewTask(sprintBoard[assignee], assignee, id, title, status, points);
+  }
+
+  function proceedCommantToTaskBoard(input) {
+    const [command, assignee, ...params] = input.split(":");
+    let tasks = sprintBoard[assignee];
+    if (!tasks) {
+      console.log(`Assignee ${assignee} does not exist on the board!`);
     } else {
-      const [command, assignee, ...params] = input.split(":");
-      let tasks = sprintBoard[assignee];
-      if (!tasks) {
-        console.log(`Assignee ${assignee} does not exist on the board!`);
-      } else {
-        sprintBoard[command](tasks, assignee, ...params);
-      }
+      sprintBoard[command](tasks, assignee, ...params);
     }
-  });
+  }
 
-  const result = Object.values(sprintBoard)
-    .filter((v) => Array.isArray(v))
-    .reduce(
-      (acc, curr) => {
-        curr.forEach((t) => {
-          acc[t.status] += t.points;
-          if (t.status !== "Done") {
-            acc.undonePoints += t.points;
-          }
-        });
-        return acc;
-      },
-      {
-        ToDo: 0,
-        "In Progress": 0,
-        "Code Review": 0,
-        Done: 0,
-        undonePoints: 0,
+  function printResult(sprintBoard) {
+    const statusPointsResult = Object.values(sprintBoard)
+      .filter((v) => Array.isArray(v))
+      .reduce(
+        (acc, curr) => {
+          curr.forEach((t) => {
+            acc[t.status] += t.points;
+            if (t.status !== "Done") {
+              acc.undonePoints += t.points;
+            }
+          });
+          return acc;
+        },
+        {
+          ToDo: 0,
+          "In Progress": 0,
+          "Code Review": 0,
+          Done: 0,
+          undonePoints: 0,
+        }
+      );
+
+    Object.entries(statusPointsResult).forEach(([status, points]) => {
+      if (status !== "undonePoints") {
+        status = status === "Done" ? "Done Points" : status;
+        console.log(`${status}: ${points}pts`);
       }
-    );
+    });
 
-  Object.entries(result).forEach(([k, v]) => {
-    if (k !== "undonePoints") {
-      k = k === "Done" ? "Done Points" : k;
-      console.log(`${k}: ${v}pts\n`);
-    }
-  });
+    var sprintResult =
+      statusPointsResult.Done >= statusPointsResult.undonePoints
+        ? "successful!"
+        : "unsuccessful...";
 
-  var sprintResult =
-    result.Done >= result.undonePoints ? "successful!" : "unsuccessful...";
-
-  console.log(`Sprint was ${sprintResult}`);
+    console.log(`Sprint was ${sprintResult}`);
+  }
 
   function addNewTask(tasks, assignee, id, title, status, points) {
     tasks.push(new Task(id, title, status, Number(points), assignee));
@@ -84,11 +94,14 @@ function solve(inputArr) {
   }
 
   function removeTask(tasks, assignee, indexStr) {
-    const index = Number(indexStr);
-    if (index < 0 || index >= tasks.length) {
+    const taskIndex = Number(indexStr);
+
+    if (taskIndex < 0 || taskIndex >= tasks.length) {
       console.log("Index is out of range!");
+      return;
     }
-    tasks.splice(index, 1);
+
+    tasks.splice(taskIndex, 1);
   }
 }
 
