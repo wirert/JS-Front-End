03b152420ca3d6857d1f79fb669cc56b14e3901a
Krasimir Nikolov <wirert@gmail.com>
2023-12-12 21:04:38 +0200

    08.03.01 Horse racing

---
+++ 08.Exam-Preps/03.Exam - 20 Apr 2023/01.HorseRacing/app.js
@@ -0,0 +1,63 @@
+function solve(input) {
+  const horses = input.shift().split("|");
+
+  commands = {
+    Retake: (winner, loser) => {
+      let winIndex = horses.indexOf(winner);
+      let loserIndex = horses.indexOf(loser);
+      if (winIndex < loserIndex) {
+        horses[winIndex] = loser;
+        horses[loserIndex] = winner;
+        console.log(`${winner} retakes ${loser}.`);
+      }
+    },
+    Trouble: (name) => {
+      const index = horses.indexOf(name);
+      if (index > 0) {
+        horses[index] = horses[index - 1];
+        horses[index - 1] = name;
+        console.log(`Trouble for ${name} - drops one position.`);
+      }
+    },
+    Rage: (name) => {
+      const index = horses.indexOf(name);
+      if (index + 2 < horses.length) {
+        horses.splice(index, 1);
+        horses.splice(index + 2, 0, name);
+      } else if (index + 2 === horses.length) {
+        horses[index] = horses[index + 1];
+        horses[index + 1] = name;
+      }
+
+      console.log(`${name} rages 2 positions ahead.`);
+    },
+    Miracle: () => {
+      const first = horses[0];
+      horses.splice(0, 1);
+      horses.push(first);
+      console.log(`What a miracle - ${first} becomes first.`);
+    },
+  };
+
+  input.forEach((line) => {
+    if (line === "Finish") {
+      return printResult();
+    }
+    const [command, ...params] = line.split(" ");
+    commands[command](...params);
+  });
+
+  function printResult() {
+    console.log(horses.join("->"));
+    console.log(`The winner is: ${horses[horses.length - 1]}`);
+  }
+}
+
+solve([
+  "Fancy|Lilly",
+  "Retake Lilly Fancy",
+  "Trouble Lilly",
+  "Trouble Lilly",
+  "Finish",
+  "Rage Lilly",
+]);
